10d0f12b35a19c0f9c99d4bf650e4ad71f26beac
jacobgonzalez0 <jacob.e.gonzalez2@gmail.com>
2022-10-12 04:16:59 -0500

    Flats now ES6, exports to image

--- packages/src/main.js
+++ packages/src/main.js
@@ -1,19 +1,24 @@
 const Wad = require("./wad")
 const Graphic  = require('./wad/graphic')
 const MapData  = require('./wad/mapdata')
+const Flat  = require('./wad/flat')
 const fs = require("fs")
 const { parse } = require("path")
 
 let parser = new Wad("DOOM1.WAD")
 let g = parser.getLump(366
     )
+let f = parser.getLump(1267
+    )
 
 let test = new Graphic(g)
+let flat = new Flat(f)
 
 let maptest = new MapData(parser, "E1M4")
 console.log(maptest.getImageData())
 fs.writeFileSync('./image2.png', test.getImageData(parser, 1))
 fs.writeFileSync('./map.png', maptest.getImageData())
+fs.writeFileSync('./flat.png', flat.getImageData(parser))
 fs.writeFileSync('./image.png', parser.playpal.getImageData(parser, 1))
 // let parser = new Wad().load(file)
 // console.log(parser.getLump(4))
--- packages/src/wad/flat.js
+++ packages/src/wad/flat.js
@@ -1,39 +1,46 @@
-var Flat = {
+const { createCanvas, loadImage } = require('canvas')
+class Flat{
     
-    data : null,
+    constructor(data){
+        this.data = null;
+        this.load(data);
+    }
     
-    load : function (lumpData) {
+    
+    load(lumpData) {
         var dv = new DataView(lumpData);
         this.data = [];
         for (var j = 0; j < 4096; j++) {
             this.data.push(dv.getUint8(j));
         }
-    },
+    }
     
     
-    toCanvas : function (wad) {
-        var scaleSize = 3;
-        var canvas = document.createElement("canvas");
-        canvas.width = 64 * scaleSize;
-        canvas.height = 64 * scaleSize;
+    getImageData(wad) {
+        const canvas = createCanvas(
+            64, 
+            64)
         var context = canvas.getContext("2d");
         var imageData = context.createImageData(64,64);
         for (var i = 0; i < 4096; i++) {
-            col = hexToRgb(wad.playpal.palettes[0][this.data[i]]);
+            let col = this.hexToRgb(wad.playpal.palettes[0][this.data[i]]);
             imageData.data[(i*4)+0] = col.r;
             imageData.data[(i*4)+1] = col.g;
             imageData.data[(i*4)+2] = col.b;
             imageData.data[(i*4)+3] = 255;
         }
-        var newCanvas = document.createElement("CANVAS");
-        newCanvas.width = imageData.width;
-        newCanvas.height = imageData.height;
-        newCanvas.getContext("2d").putImageData(imageData, 0, 0);
-        context.scale(scaleSize, scaleSize);
-        context.mozImageSmoothingEnabled = false;
-        context.msImageSmoothingEnabled = false;
-        context.imageSmoothingEnabled = false;
-        context.drawImage(newCanvas, 0, 0);
-        return canvas;
+        context.putImageData(imageData, 0, 0, 0, 0, canvas.width, canvas.height)
+        const buffer = canvas.toBuffer('image/png')
+        return buffer;
+    }
+
+    hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
     }
 }
+module.exports = Flat;
